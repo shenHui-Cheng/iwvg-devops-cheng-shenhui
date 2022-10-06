f02c36871b729ad966cbf751a49e1691aa4c1fda
shenhui cheng <shenhui.cheng@alumnos.upm.es>
2022-10-06 23:00:32 +0200

    added findDecimalFractionByUserName and its test for issue #15 at issue#15 branch

--- src/main/java/es/upm/miw/iwvg_devops/code/Searches.java
+++ src/main/java/es/upm/miw/iwvg_devops/code/Searches.java
@@ -15,4 +15,10 @@ public class Searches {
                 .map(User::getFamilyName);
     }
 
+    public Stream<Double> findDecimalFractionByUserName(String name){
+        return new UsersDatabase().findAll()
+                .filter(user -> name.equals(user.getName()))
+                .flatMap(user -> user.getFractions().stream())
+                .map(Fraction::decimal);
+    }
 }
--- src/test/java/es/upm/miw/iwvg_devops/code/SearchesTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/code/SearchesTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SearchesTest {
@@ -19,4 +21,10 @@ public class SearchesTest {
         assertEquals(Arrays.asList("Fernandez","Blanco","López","Blanco"),new Searches().findUserFamilyNameInitialBySomeProperFraction().collect(Collectors.toList()));
     }
 
+    @Test
+    void testFindDecimalFractionByUserName() {
+        List list = List.of(0.0,1.0,2.0,0.2,-0.5,0.5,1.0);
+        Stream<Double> stream = new Searches().findDecimalFractionByUserName("Oscar");
+        assertEquals(list,stream.collect(Collectors.toList()));
+    }
 }
